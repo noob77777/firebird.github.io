3f65bed1bc3bde5c77d28904a1a32cd189831905
Abhishek Pal <42897033+noob77777@users.noreply.github.com>
2019-12-30 18:37:16 +0530

    Add files via upload

--- database.js
+++ database.js
@@ -1,7 +1,6 @@
 UserId = "";
 receiver = "undefined";
-(function() {
-  // Your web app's Firebase configuration
+
   var firebaseConfig = {
     apiKey: "AIzaSyA6wxeT-E28I87gZG2X1lKdv52X5GF2BIU",
     authDomain: "firebird-2204b.firebaseapp.com",
@@ -19,6 +18,67 @@ receiver = "undefined";
     .ref()
     .child("Root");
 
+function live_inbox_feed(snap) {
+      list = snap.val();
+      const ul = document.getElementById("live_inbox");
+      ul.innerHTML = "";
+      for (i = Math.max(1, list.length-12); i < list.length; i++) {
+        var li = document.createElement("li");
+        if(list[i].receiver == receiver || list[i].sender == receiver){
+          li.innerText = JSON.stringify(list[i]);
+          ul.appendChild(li);
+        }
+      }
+    }
+
+  function selectChat() {
+      const SendTag = document.getElementById("sender");
+  const RTag = document.getElementById("receiver");
+    receiver = this.innerHTML;
+    RTag.innerHTML = receiver;
+
+        
+
+    dbRef
+      .child(UserId)
+      .child("inbox")
+      .on("value", live_inbox_feed);
+
+  }
+
+function update_contacts(list){
+  const node = document.getElementById("contacts");
+  node.innerHTML = "";
+  for(i = 1; i < list.length; i++){
+    var li = document.createElement("li");
+    var btn = document.createElement("button");
+    btn.onclick = selectChat;
+    btn.innerHTML = list[i];
+    li.appendChild(btn);
+    node.appendChild(li);
+  }
+}
+
+
+(function() {
+  // Your web app's Firebase configuration
+  // var firebaseConfig = {
+  //   apiKey: "AIzaSyA6wxeT-E28I87gZG2X1lKdv52X5GF2BIU",
+  //   authDomain: "firebird-2204b.firebaseapp.com",
+  //   databaseURL: "https://firebird-2204b.firebaseio.com",
+  //   projectId: "firebird-2204b",
+  //   storageBucket: "firebird-2204b.appspot.com",
+  //   messagingSenderId: "182608640887",
+  //   appId: "1:182608640887:web:652f17a5cd1ab4fd6bbf72"
+  // };
+  // // Initialize Firebase
+  // firebase.initializeApp(firebaseConfig);
+
+  // const dbRef = firebase
+  //   .database()
+  //   .ref()
+  //   .child("Root");
+
   // function read(snap) {
   //   const node = document.getElementById("live_mssg");
   //   node.innerHTML = snap.val().name;
@@ -32,12 +92,13 @@ receiver = "undefined";
   const RTag = document.getElementById("receiver");
 
   INBOX = ["begin"];
+  CONTACTS = ["begin"];
 
   function login() {
     UserId = input.value;
     const node = document.getElementById("loginmenu");
     // node.style.visibility = "hidden";
-    node.innerHTML = "<h1>Welcome to FireBird</h1>";
+    node.innerHTML = "<h1 id='header'>Welcome to FireBird</h1>";
     SendTag.innerHTML = UserId;
     RTag.innerHTML = receiver;
 
@@ -45,11 +106,18 @@ receiver = "undefined";
       flag = snap.exists();
       if (!flag) {
         dbRef.child(UserId).set({
-          inbox: INBOX
+          inbox: INBOX,
+          contacts: CONTACTS
         });
       }
       if (flag) {
-        console.log("Exists");
+        list = [];
+        function local_reader(snap){
+          list = snap.val();
+          update_contacts(list);
+        }
+        dbRef.child(UserId).child("contacts").once("value", local_reader);
+        
       }
 
       const node2 = document.getElementById("interface");
@@ -58,29 +126,19 @@ receiver = "undefined";
 
     IN = dbRef.child(UserId).once("value", local_reader);
 
-    function live_inbox_feed(snap) {
-      list = snap.val();
-      const ul = document.getElementById("live_inbox");
-      for (i = 1; i < list.length; i++) {
-        var li = document.createElement("li");
-        li.innerText = JSON.stringify(list[i]);
-        ul.appendChild(li);
-      }
-    }
+    
     dbRef
       .child(UserId)
       .child("inbox")
       .on("value", live_inbox_feed);
+
   }
   btn.onclick = login;
 
   const newIdBtn = document.getElementById("newuser");
   const inputNewUser = document.getElementById("newid");
 
-  function selectChat() {
-    receiver = this.innerHTML;
-    RTag.innerHTML = receiver;
-  }
+
 
   function createNewChat() {
     var li = document.createElement("li");
@@ -88,8 +146,16 @@ receiver = "undefined";
     temp.onclick = selectChat;
     temp.innerHTML = inputNewUser.value;
     li.appendChild(temp);
-    var ul = document.getElementById("list");
+    var ul = document.getElementById("contacts");
     ul.appendChild(li);
+
+    function local_reader(snap){
+      list = snap.val();
+      list.push(inputNewUser.value);
+      dbRef.child(UserId).child("contacts").set(list);
+    }
+    dbRef.child(UserId).child("contacts").once("value", local_reader);
+
   }
 
   newIdBtn.onclick = createNewChat;
@@ -110,11 +176,23 @@ receiver = "undefined";
       function update_inbox(snap) {
         list = snap.val();
         // console.log(list);
-        list.push({ sender: UserId, message: text });
+        list.push({ sender: UserId, message: text, receiver: receiver});
         // console.log(list);
 
         dbRef
           .child(receiver)
+          .child("inbox")
+          .set(list);
+      }
+
+      function update_inbox_self(snap){
+              list = snap.val();
+        // console.log(list);
+        list.push({ sender: UserId, message: text, receiver: receiver});
+        // console.log(list);
+
+        dbRef
+          .child(UserId)
           .child("inbox")
           .set(list);
       }
@@ -125,6 +203,10 @@ receiver = "undefined";
           .child(receiver)
           .child("inbox")
           .once("value", update_inbox);
+
+        dbRef.child(UserId)
+        .child("inbox")
+        .once("value", update_inbox_self);  
       }
     }
 
