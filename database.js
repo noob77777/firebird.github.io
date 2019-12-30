52bf14a2a5283bf200b8a1481714f3f0f02f1229
Abhishek Pal <42897033+noob77777@users.noreply.github.com>
2019-12-30 23:22:01 +0530

    Add files via upload

--- database.js
+++ database.js
@@ -1,65 +1,75 @@
 UserId = "";
 receiver = "undefined";
 
-  var firebaseConfig = {
-    apiKey: "AIzaSyA6wxeT-E28I87gZG2X1lKdv52X5GF2BIU",
-    authDomain: "firebird-2204b.firebaseapp.com",
-    databaseURL: "https://firebird-2204b.firebaseio.com",
-    projectId: "firebird-2204b",
-    storageBucket: "firebird-2204b.appspot.com",
-    messagingSenderId: "182608640887",
-    appId: "1:182608640887:web:652f17a5cd1ab4fd6bbf72"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+//
+// FIREBASE INIT
+var firebaseConfig = {
+  apiKey: "AIzaSyA6wxeT-E28I87gZG2X1lKdv52X5GF2BIU",
+  authDomain: "firebird-2204b.firebaseapp.com",
+  databaseURL: "https://firebird-2204b.firebaseio.com",
+  projectId: "firebird-2204b",
+  storageBucket: "firebird-2204b.appspot.com",
+  messagingSenderId: "182608640887",
+  appId: "1:182608640887:web:652f17a5cd1ab4fd6bbf72"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-  const dbRef = firebase
-    .database()
-    .ref()
-    .child("Root");
+const dbRef = firebase
+  .database()
+  .ref()
+  .child("Root");
 
-function formatted(obj){
+//
+//
+//
+
+function formatted(obj) {
   type = "";
-  if(obj.sender == UserId){
+  if (obj.sender == UserId) {
     type = "right";
-  }else{
+  } else {
     type = "left";
   }
-  return `<h5 id=${type}>${obj.message}</h3>`
+  return `<h5 id=${type}>${obj.message} <span class="text-secondary css-sm">~${obj.sender}</span></h5>`;
 }
 
 function live_inbox_feed(snap) {
-      list = snap.val();
-      const ul = document.getElementById("live_inbox");
-      ul.innerHTML = "";
-      for (i = Math.max(1, list.length-12); i < list.length; i++) {
-        var li = document.createElement("li");
-        if(list[i].receiver == receiver || list[i].sender == receiver){
-          li.innerHTML = formatted(list[i]);//JSON.stringify(list[i]);
-          ul.appendChild(li);
-        }
-      }
+  list = snap.val();
+  const ul = document.getElementById("live-inbox");
+  ul.innerHTML = "";
+  for (i = Math.max(1, list.length - 12); i < list.length; i++) {
+    var li = document.createElement("li");
+    if (
+      list[i].receiver == receiver ||
+      list[i].sender == receiver ||
+      receiver == "undefined"
+    ) {
+      li.innerHTML = formatted(list[i]); //JSON.stringify(list[i]);
+      ul.appendChild(li);
     }
-
-  function selectChat() {
-      const SendTag = document.getElementById("sender");
-  const RTag = document.getElementById("receiver");
-    receiver = this.innerHTML;
-    RTag.innerHTML = receiver;
-
-        
-
-    dbRef
-      .child(UserId)
-      .child("inbox")
-      .on("value", live_inbox_feed);
-
   }
+}
 
-function update_contacts(list){
+function selectChat() {
+  const SendTag = document.getElementById("sender");
+  const RTag = document.getElementById("receiver");
+  receiver = this.innerHTML;
+  RTag.innerHTML = receiver;
+
+  const node3 = document.getElementById("sending-utility");
+  node3.style.visibility = "visible";
+
+  dbRef
+    .child(UserId)
+    .child("inbox")
+    .on("value", live_inbox_feed);
+}
+
+function update_contacts(list) {
   const node = document.getElementById("contacts");
   node.innerHTML = "";
-  for(i = 1; i < list.length; i++){
+  for (i = 1; i < list.length; i++) {
     var li = document.createElement("li");
     var btn = document.createElement("button");
     btn.onclick = selectChat;
@@ -69,87 +79,101 @@ function update_contacts(list){
   }
 }
 
-
 (function() {
-  // Your web app's Firebase configuration
-  // var firebaseConfig = {
-  //   apiKey: "AIzaSyA6wxeT-E28I87gZG2X1lKdv52X5GF2BIU",
-  //   authDomain: "firebird-2204b.firebaseapp.com",
-  //   databaseURL: "https://firebird-2204b.firebaseio.com",
-  //   projectId: "firebird-2204b",
-  //   storageBucket: "firebird-2204b.appspot.com",
-  //   messagingSenderId: "182608640887",
-  //   appId: "1:182608640887:web:652f17a5cd1ab4fd6bbf72"
-  // };
-  // // Initialize Firebase
-  // firebase.initializeApp(firebaseConfig);
-
-  // const dbRef = firebase
-  //   .database()
-  //   .ref()
-  //   .child("Root");
-
-  // function read(snap) {
-  //   const node = document.getElementById("live_mssg");
-  //   node.innerHTML = snap.val().name;
-  // }
-
-  // dbRef.on("value", read);
-
-  const btn = document.getElementById("submit_button");
-  const input = document.getElementById("user");
+  const btn = document.getElementById("submit-button");
+  const input = document.getElementById("inputEmail");
+  const pass = document.getElementById("inputPassword");
   const SendTag = document.getElementById("sender");
   const RTag = document.getElementById("receiver");
 
   INBOX = ["begin"];
   CONTACTS = ["begin"];
 
+  //
+  //
+  // LOGIN
+  // BAD CODE DO NOT TOUCH
+  //
   function login() {
     UserId = input.value;
-    const node = document.getElementById("loginmenu");
-    // node.style.visibility = "hidden";
-    node.innerHTML = "<h1 id='header'>Welcome to FireBird</h1>";
-    SendTag.innerHTML = UserId;
-    RTag.innerHTML = receiver;
+    password = pass.value;
+
+    // console.log([UserId, password]);
 
     function local_reader(snap) {
-      flag = snap.exists();
-      if (!flag) {
-        dbRef.child(UserId).set({
-          inbox: INBOX,
-          contacts: CONTACTS
-        });
-      }
-      if (flag) {
-        list = [];
-        function local_reader(snap){
-          list = snap.val();
-          update_contacts(list);
+      flag = false;
+      // console.log();
+      if (snap.exists()) {
+        if (snap.val().password == password) {
+          flag = true;
         }
-        dbRef.child(UserId).child("contacts").once("value", local_reader);
-        
+      } else {
+        flag = true;
       }
 
-      const node2 = document.getElementById("interface");
-      node2.style.visibility = "visible";
+      // console.log(flag);
+
+      //
+      // IF LOGIN
+      //
+      if (flag) {
+        const node = document.getElementById("loginmenu");
+        node.innerHTML = "<h1 id='header'>Welcome to FireBird.</h1>";
+        SendTag.innerHTML = UserId;
+        RTag.innerHTML = receiver;
+
+        function local_reader(snap) {
+          flag = snap.exists();
+          if (!flag) {
+            dbRef.child(UserId).set({
+              inbox: INBOX,
+              contacts: CONTACTS,
+              password: password
+            });
+          }
+          if (flag) {
+            list = [];
+            function local_reader(snap) {
+              list = snap.val();
+              update_contacts(list);
+            }
+            dbRef
+              .child(UserId)
+              .child("contacts")
+              .once("value", local_reader);
+          }
+
+          const node2 = document.getElementById("interface");
+          node2.style.visibility = "visible";
+          const node3 = document.getElementById("sending-utility");
+          node3.style.visibility = "hidden";
+        }
+
+        dbRef.child(UserId).once("value", local_reader);
+        dbRef
+          .child(UserId)
+          .child("inbox")
+          .on("value", live_inbox_feed);
+      }
+      //
+      // AUTH FAIL
+      //
+      else {
+        const node = document.getElementById("AuthFail");
+        node.innerHTML = "Invalid Username or Password";
+      }
     }
-
-    IN = dbRef.child(UserId).once("value", local_reader);
-
-    
-    dbRef
-      .child(UserId)
-      .child("inbox")
-      .on("value", live_inbox_feed);
-
+    dbRef.child(UserId).once("value", local_reader);
   }
   btn.onclick = login;
 
+  //
+  //
+  // ADD CONTACTS
+  //
+  //
   const newIdBtn = document.getElementById("newuser");
   const inputNewUser = document.getElementById("newid");
-
-
-
   function createNewChat() {
     var li = document.createElement("li");
     var temp = document.createElement("button");
@@ -159,20 +183,29 @@ function update_contacts(list){
     var ul = document.getElementById("contacts");
     ul.appendChild(li);
 
-    function local_reader(snap){
+    function local_reader(snap) {
       list = snap.val();
       list.push(inputNewUser.value);
-      dbRef.child(UserId).child("contacts").set(list);
+      dbRef
+        .child(UserId)
+        .child("contacts")
+        .set(list);
     }
-    dbRef.child(UserId).child("contacts").once("value", local_reader);
-
+    dbRef
+      .child(UserId)
+      .child("contacts")
+      .once("value", local_reader);
   }
-
   newIdBtn.onclick = createNewChat;
 
-  const SEND = document.getElementById("send_mssg");
+  //
+  //
+  // THE SEND BUTTON
+  //
+  //
+  const SEND = document.getElementById("send-mssg");
   function sendMessage() {
-    const text = document.getElementById("live_mssg").value;
+    const text = document.getElementById("live-mssg").value;
     dbRef
       .child(UserId)
       .child(receiver)
@@ -186,7 +219,7 @@ function update_contacts(list){
       function update_inbox(snap) {
         list = snap.val();
         // console.log(list);
-        list.push({ sender: UserId, message: text, receiver: receiver});
+        list.push({ sender: UserId, message: text, receiver: receiver });
         // console.log(list);
 
         dbRef
@@ -195,10 +228,10 @@ function update_contacts(list){
           .set(list);
       }
 
-      function update_inbox_self(snap){
-              list = snap.val();
+      function update_inbox_self(snap) {
+        list = snap.val();
         // console.log(list);
-        list.push({ sender: UserId, message: text, receiver: receiver});
+        list.push({ sender: UserId, message: text, receiver: receiver });
         // console.log(list);
 
         dbRef
@@ -208,21 +241,21 @@ function update_contacts(list){
       }
 
       if (flag) {
-        console.log("OK");
+        // console.log("OK");
         dbRef
           .child(receiver)
           .child("inbox")
           .once("value", update_inbox);
 
-        dbRef.child(UserId)
-        .child("inbox")
-        .once("value", update_inbox_self);  
+        dbRef
+          .child(UserId)
+          .child("inbox")
+          .once("value", update_inbox_self);
       }
     }
 
-    IN = dbRef.child(receiver).once("value", local_reader);
-
-    document.getElementById("live_mssg").value = "";
+    dbRef.child(receiver).once("value", local_reader);
+    document.getElementById("live-mssg").value = "";
   }
   SEND.onclick = sendMessage;
 })();
