eb96e6beb3b9987ee12f2e84225fe5daed4fc814
Abhishek Pal <42897033+noob77777@users.noreply.github.com>
2020-01-01 23:38:18 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //script
+(function() {
+  const panel = document.getElementById("contact-panel");
+  const inbox = document.getElementById("inbox");
+  //   panel.style.visibility = "hidden";
+})();
