4f0ffc6b783aafa3efe23faa145467e255c8da4e
Abhishek Pal <42897033+noob77777@users.noreply.github.com>
2020-01-02 00:43:13 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 //script
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "50vw";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
 (function() {
-  const panel = document.getElementById("contact-panel");
   const inbox = document.getElementById("inbox");
-  //   panel.style.visibility = "hidden";
+  const btn = document.getElementById("contact-open");
+  const panel = document.getElementById("remove");
+  const content = document.getElementById("side-content");
+  if (screen.width < screen.height) {
+    btn.innerHTML = "";
+  } else {
+    panel.innerHTML = "";
+    content.innerHTML =
+      "<div id='contact-panel' class='col-4'><h2>Contacts</h2></div >";
+    inbox.classList.add("col-12");
+  }
 })();
