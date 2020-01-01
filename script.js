c675682e7681e9dc9a3f11d8c8b6c6e826ee6060
Abhishek Pal <42897033+noob77777@users.noreply.github.com>
2020-01-02 01:20:37 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -14,12 +14,17 @@ function closeNav() {
   const btn = document.getElementById("contact-open");
   const panel = document.getElementById("remove");
   const content = document.getElementById("side-content");
+  const mssg_input = document.getElementById("live-mssg-div");
   if (screen.width > screen.height) {
     btn.innerHTML = "";
   } else {
     panel.innerHTML = "";
+    panel.classList.remove("col-4");
     content.innerHTML =
-      "<div id='contact-panel' class='col-4'><h2>Contacts</h2></div >";
+      '<div id="contact-panel" class="col-12"><h2>Contacts</h2>Receivers ID:<br /><input type="text" class="form-control" id="newid" /><button id="newuser" class="btn-info btn">Create New</button><ul id="contacts"></ul></div >';
+    inbox.classList.remove("col-8");
     inbox.classList.add("col-12");
+    // mssg_input.classList.remove("col-8");
+    // mssg_input.classList.add("col-10");
   }
 })();
